d8c8818e5ebf2a0f52d5794e21f6a8b101dab8d3
yayaya7639 <izu14587@kwansei.ac.jp>
2021-04-24 15:02:42 +0900

    modified html & add css

--- abstransproject/settings.py
+++ abstransproject/settings.py
@@ -119,4 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# CSSファイルはstaticディレクトリ下にある
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
